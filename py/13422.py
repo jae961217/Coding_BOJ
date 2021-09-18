cebadc5578f3f79e1ec295d93b46179e956b9f0f
jae961217 <jae961217@gmail.com>
2021-09-18 11:43:05 +0900

    투 포인터

--- py/13422.py
+++ py/13422.py
@@ -3,10 +3,20 @@ input=sys.stdin.readline
 for tc in range(int(input())):
     n,m,k=map(int,input().split())
     numbers=list(map(int,input().split()))
-    res=0
-    for i in range(m):
-        numbers.append(numbers[i])
-    for i in range(n):
-        if sum(numbers[i:i+m]) < k:
+    arr=[]
+    res, tmp=0, 0
+    for i in numbers:
+        tmp+=i
+        arr.append(tmp)
+    if n==m:
+        if sum(numbers)<k:
             res+=1
+    else:
+        for i in range(m):
+            tmp+=numbers[i]
+            arr.append(tmp)
+        for i in range(n):
+            if arr[m+i]-arr[i]<k:
+                res+=1
+
     print(res)
